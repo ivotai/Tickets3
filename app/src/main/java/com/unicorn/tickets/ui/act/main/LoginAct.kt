b93dce0347cc2cdd5e9d5e3b92b4a86ab2588b34
ivotai <ivotai@163.com>
2022-06-19 08:44:19 +0800

    CaptchaDialog 初

--- app/src/main/java/com/unicorn/tickets/ui/act/main/LoginAct.kt
+++ app/src/main/java/com/unicorn/tickets/ui/act/main/LoginAct.kt
@@ -1,20 +1,20 @@
 package com.unicorn.tickets.ui.act.main
 
 import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
 import com.afollestad.materialdialogs.list.listItemsSingleChoice
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.PhoneUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.github.florent37.rxsharedpreferences.RxSharedPreferences
+import com.unicorn.tickets.R
 import com.unicorn.tickets.app.*
 import com.unicorn.tickets.app.helper.DialogHelper
 import com.unicorn.tickets.app.helper.ExceptionHelper
 import com.unicorn.tickets.app.helper.UpdateHelper
 import com.unicorn.tickets.ui.base.BaseAct
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.act_login.*
-import java.util.concurrent.TimeUnit
 
 class LoginAct : BaseAct() {
 
@@ -57,8 +57,8 @@ class LoginAct : BaseAct() {
             .observeOnMain(this)
             .subscribeBy(
                 onSuccess = {
-                    if (it.failed) return@subscribeBy
-                    loginX()
+//                    if (it.failed) return@subscribeBy
+                    login()
                 },
                 onError = {
                     ExceptionHelper.showPrompt(it)
@@ -66,35 +66,7 @@ class LoginAct : BaseAct() {
             )
     }
 
-    // login 的加强版
-    private fun loginX() {
-        fun saveUserInfo() {
-            RxSharedPreferences.with(this).apply {
-                putString(Key.Username, etUsername.trimText()).subscribe { }
-                putString(Key.Password, etPassword.trimText()).subscribe { }
-                putBoolean(Key.KeepPwd, cbKeepPwd.isChecked).subscribe { }
-            }
-        }
-
-        fun login() {
-            val mask = DialogHelper.showMask(this)
-            api.login(etUsername.trimText(), etPassword.trimText())
-                .observeOnMain(this)
-                .subscribeBy(
-                    onSuccess = {
-                        mask.dismiss()
-                        if (it.failed) return@subscribeBy
-                        Global.loginResponse = it
-                        saveUserInfo()
-                        UpdateHelper.checkVersion(this)
-                    },
-                    onError = {
-                        mask.dismiss()
-                        ExceptionHelper.showPrompt(it)
-                    }
-                )
-        }
-
+    private fun login() {
         if (etUsername.isEmpty()) {
             ToastUtils.showShort("工号不能为空")
             return
@@ -103,7 +75,40 @@ class LoginAct : BaseAct() {
             ToastUtils.showShort("密码不能为空")
             return
         }
-        login()
+        showCaptchaDialog()
+    }
+
+    private fun showCaptchaDialog(){
+        MaterialDialog(this).show {
+            customView(R.layout.dialog_captcha)
+        }
+    }
+
+    private fun loginReal() {
+        fun saveUserInfo() {
+            RxSharedPreferences.with(this).apply {
+                putString(Key.Username, etUsername.trimText()).subscribe { }
+                putString(Key.Password, etPassword.trimText()).subscribe { }
+                putBoolean(Key.KeepPwd, cbKeepPwd.isChecked).subscribe { }
+            }
+        }
+
+        val mask = DialogHelper.showMask(this)
+        api.login(etUsername.trimText(), etPassword.trimText())
+            .observeOnMain(this)
+            .subscribeBy(
+                onSuccess = {
+                    mask.dismiss()
+                    if (it.failed) return@subscribeBy
+                    Global.loginResponse = it
+                    saveUserInfo()
+                    UpdateHelper.checkVersion(this)
+                },
+                onError = {
+                    mask.dismiss()
+                    ExceptionHelper.showPrompt(it)
+                }
+            )
     }
 
     override val layoutId = com.unicorn.tickets.R.layout.act_login
